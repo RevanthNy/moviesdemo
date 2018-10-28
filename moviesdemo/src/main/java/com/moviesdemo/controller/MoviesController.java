5e4ae8553fbacc9b9139f1cab528ad4959a53a54
Revanth Reddy <Revanth Reddy@DESKTOP-UQJBG23>
2018-10-28 12:36:47 -0500

    properties changed

--- moviesdemo/src/main/java/com/moviesdemo/controller/MoviesController.java
+++ moviesdemo/src/main/java/com/moviesdemo/controller/MoviesController.java
@@ -28,7 +28,7 @@ import com.moviesdemo.repository.MoviesRepository;
  *
  */
 @Controller    // This means that this class is a Controller
-@RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
+@RequestMapping(path="/lvlworkshop") // This means URL's start with /demo (after Application path)
 public class MoviesController {
 	 final static Logger logger = LoggerFactory.getLogger(MoviesController.class);
 	@Autowired // This means to get the bean called moviesRepository
